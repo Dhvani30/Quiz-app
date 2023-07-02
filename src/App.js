95a30be51300642225488ae5f023d742a2a64ef3
Dhvani <dkm12305@gmail.com>
2023-07-02 18:23:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React from "react";
+import { Particles } from 'react-tsparticles';
 // import { VscHome, VscPlay, VscSettingsGear, VscArrowLeft, VscAccount, VscMenu } from "react-icons/vsc";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import {BrowserRouter as Router,Route, Link, Routes, BrowserRouter} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Side from "./components/Side";
 import Setting from "./components/pages/Setting";
 import Play from "./components/pages/Play";
 import Score from "./components/pages/Score";
-// import './index.css';
+import Design from "./Design";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Side>
-        <Routes>
-          <Route path="/" element={<Side />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/play" element={<Play />} />
-          <Route path="/settings" element={<Setting />} />
-          <Route path="/score" element={<Score/>} />
-        </Routes>
-
-      </Side>
-    </BrowserRouter>
+    <div className="w-full h-screen bg-gray-800 bg-no-repeat bg-center relative overflow-hidden">
+      <div className="max-w-6x1 mx-auto text-white">
+        <BrowserRouter>
+          <Side />
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/play" element={<Play />} />
+              <Route path="/settings" element={<Setting />} />
+              <Route path="/score" element={<Score />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+        <Design />
+      </div>
+    </div>
   );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
 const Home = () => {
-    return (
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login/authentication logic with username and password
+    // You can use the entered username and password in this function
+  };
+
+  return (
+    <div className="home">
+      <h1>Welcome to the Quiz App!</h1>
+      <p>Test your knowledge with our fun quizzes.</p>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h1>Home</h1>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
         </div>
-    )
-}
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <Link to="/play">Start Quiz</Link>
+    </div>
+  );
+};
 
 
-export default Home
+
+export default Home;
